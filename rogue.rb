8743697022b80b1d729b8831839cbd27e3382419
Polar Humenn <polar@adiron.com>
2013-05-11 13:12:34 -0400

    update

--- rogue.rb
+++ rogue.rb
@@ -122,6 +122,7 @@ logger.add "About to Run Swift"
 begin
   logger.add "Int to Run Swift"
   Swiftcore::Swiftiply.run(config)
+  sleep 234234234234234
   logger.add "Out Swift"
 rescue Exception => boom
  logger.add "#{boom}"
