68b29c8c14e55a0da6b0096bce59866eeab698f5
Polar Humenn <polar@adiron.com>
2013-05-15 14:10:46 -0400

    update

---
+++ boot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd ~ec2-user
+
+cd ./busme-swifty
+
+git pull
+
+chown -R ec2-user:ec2-user .
+
+cp nginx.conf /etc/nginx
+/etc/init.d/nginx restart
+
+mkdir -p /var/log/swifty
+chown ec2-user:ec2-uesr /var/log/swifty
+
+su - ec2-user <<EOF
+source ~/creds
+cd ./busme-swifty
+bundle install
+
+# busme.us
+bundle exec ruby rogue.rb --port 3000 &> /var/log/swifty/busme.log &
+
+# sites.busme.us
+bundle exec ruby rogue.rb --port 3010 &> /var/log/swifty/sites.log &
+
+# lake-shore-limited.busme.us
+bundle exec ruby rogue.rb --port 3020 &> /var/log/swifty/lake-shore-limited.log &
+
+# syracuse-university.busme.us
+bundle exec ruby rogue.rb --port 3030 &> /var/log/swifty/syracuse-university.log &
+
+# apis.busme.us is currently hosted on Heroku due to the SSL validation issue.
+
+EOF
