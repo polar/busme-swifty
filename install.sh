adc092f298abbdb66d8c02d6f941b7ee12626c13
Polar Humenn <polar@adiron.com>
2013-05-15 17:37:44 -0400

    update

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ cd ~ec2-user/busme-swifty
 cp init.sh /etc/init.d/swifty
 
 chmod 0755 /etc/init.d/swifty
-ln -s /etc/init.d/swifty /etc/rc.d/rc0.d/K20swifty
-ln -s /etc/init.d/swifty /etc/rc.d/rc1.d/K20swifty
-ln -s /etc/init.d/swifty /etc/rc.d/rc2.d/S20swifty
-ln -s /etc/init.d/swifty /etc/rc.d/rc3.d/S20swifty
-ln -s /etc/init.d/swifty /etc/rc.d/rc4.d/S20swifty
-ln -s /etc/init.d/swifty /etc/rc.d/rc5.d/S20swifty
-ln -s /etc/init.d/swifty /etc/rc.d/rc6.d/K20swifty
+ln -s /etc/init.d/swifty /etc/rc.d/rc0.d/K20swifty   >& /dev/null
+ln -s /etc/init.d/swifty /etc/rc.d/rc1.d/K20swifty   >& /dev/null
+ln -s /etc/init.d/swifty /etc/rc.d/rc2.d/S20swifty   >& /dev/null
+ln -s /etc/init.d/swifty /etc/rc.d/rc3.d/S20swifty   >& /dev/null
+ln -s /etc/init.d/swifty /etc/rc.d/rc4.d/S20swifty   >& /dev/null
+ln -s /etc/init.d/swifty /etc/rc.d/rc5.d/S20swifty   >& /dev/null
+ln -s /etc/init.d/swifty /etc/rc.d/rc6.d/K20swifty   >& /dev/null
