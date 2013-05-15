43d6c2c4f6d4657774200c311ea67c4896ca5eb9
Polar Humenn <polar@adiron.com>
2013-05-15 14:39:09 -0400

    update

--- boot.sh
+++ boot.sh
@@ -14,6 +14,18 @@ cp nginx.conf /etc/nginx
 mkdir -p /var/log/swifty
 chown ec2-user:ec2-user /var/log/swifty
 
+mkdir -p /var/run/swifty
+chown ec2-user:ec2-user /var/run/swifty
+cd /var/run/swifty
+for i in *.pid do
+    kill -TERM `cat $i`
+end
+sleep 5
+for i in *.pid do
+    kill -KILL `cat $i`
+    rm $i
+end
+
 su - ec2-user <<EOF
 source ~/.busme_creds
 cd ./busme-swifty
@@ -21,15 +33,19 @@ bundle install
 
 # busme.us
 bundle exec ruby rogue.rb --cluster-port 3000 --backend-port 4000 &> /var/log/swifty/busme.log &
+echo "$!" > /var/run/swifty/busme.pid
 
 # sites.busme.us
 bundle exec ruby rogue.rb --master-slug sites --cluster-port 3010 --backend-port 4010 &> /var/log/swifty/sites.log &
+echo "$!" > /var/run/swifty/sites.pid
 
 # lake-shore-limited.busme.us
 bundle exec ruby rogue.rb --master-slug lake-shore-limited --cluster-port 3020 --backend-port 4020 &> /var/log/swifty/lake-shore-limited.log &
+echo "$!" > /var/run/swifty/lake-shore-limited.pid
 
 # syracuse-university.busme.us
 bundle exec ruby rogue.rb --master-slug syracuse-university --cluster-port 3030 --backend-port 4030 &> /var/log/swifty/syracuse-university.log &
+echo "$!" > /var/run/swifty/syracuse-university.pid
 
 # apis.busme.us is currently hosted on Heroku due to the SSL validation issue.
 
