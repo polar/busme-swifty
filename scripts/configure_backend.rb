c10b0b5e53739014e24e59aaddca2739693fcc11
Polar Humenn <polar@adiron.com>
2013-07-11 13:48:07 -0400

    update

--- scripts/configure_backend.rb
+++ scripts/configure_backend.rb
@@ -101,6 +101,7 @@ else
 end
 puts "Creating backend config files from #{Dir.pwd}."
 
+puts Rush.bash("ls scripts")
 Rush.bash("scripts/create_backend_configfiles.sh '#{backend.frontend.name}' '#{backend.name}' '#{backend.frontend_address}' '#{backend.master_slug}' '#{backend.hostname}' '#{backend.server_name}' '#{backend.cluster_address}' '#{backend.cluster_port}' '#{backend.address}' '#{backend.port}'")
 
 backend.configured = true
