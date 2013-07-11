459cc4bb5c47136dad75a41ee7ff0363c2161d4d
Polar Humenn <polar@adiron.com>
2013-07-11 13:03:23 -0400

    update

--- scripts/deconfigure_backend.rb
+++ scripts/deconfigure_backend.rb
@@ -14,6 +14,7 @@ end.parse!
 if ! config["name"].nil?
   backend = Backend.find_by_name(config[:name])
   if backend
+    puts "#{backend.inspect}"
     Rush["~ec2-user/busme-swifty/backends.d/#{backend.name}.conf"].destroy
     Rush["~ec2-user/busme-swifty/start.d/#{backend.name}.sh"].destroy
     puts "Backend frontend = #{backend.frontend_id}"
