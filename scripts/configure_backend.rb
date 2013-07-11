1b5ed2a47e0ab7f9f37b22f329dbba242165e2a8
Polar Humenn <polar@adiron.com>
2013-07-11 13:40:03 -0400

    update

--- scripts/configure_backend.rb
+++ scripts/configure_backend.rb
@@ -99,6 +99,7 @@ else
     exit 1
   end
 end
+puts "Creating backend config files."
 
 Rush.bash("scripts/create_backend_configfiles.sh '#{backend.frontend.name}' '#{backend.name}' '#{backend.frontend_address}' '#{backend.master_slug}' '#{backend.hostname}' '#{backend.server_name}' '#{backend.cluster_address}' '#{backend.cluster_port}' '#{backend.address}' '#{backend.port}'")
 
