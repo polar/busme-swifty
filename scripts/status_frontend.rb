4d3e15a76c8e43e2c1a52446a0ae5df7707460aa
Polar Humenn <polar@adiron.com>
2013-07-11 23:25:54 -0400

    update

--- scripts/status_frontend.rb
+++ scripts/status_frontend.rb
@@ -26,21 +26,21 @@ if ! config["name"].nil?
   frontend = Frontend.find_by_name(config["name"])
   if frontend
     frontend.listen_status = []
-    frontend.listen_status += eatme(/0.0.0.0:80.*LISTEN/, Rush.bash("netstat -tan").split("\n"))
-    frontend.listen_status += eatme(/0.0.0.0:443.*LISTEN/, Rush.bash("netstat -tan").split("\n"))
+    frontend.listen_status += eatme(/(0.0.0.0:80.*LISTEN)/, Rush.bash("netstat -tan").split("\n"))
+    frontend.listen_status += eatme(/(0.0.0.0:443.*LISTEN)/, Rush.bash("netstat -tan").split("\n"))
 
     puts "STATUS LENGTH #{frontend.listen_status.length}"
     if frontend.listen_status.length == 0
       frontend.listen_status = ["DOWN"]
     end
     frontend.connection_status = []
-    frontend.connection_status += eatme(/0.0.0.0:80.*ESTABLISHED/, Rush.bash("netstat -tan").split("\n"))
-    frontend.connection_status += eatme(/0.0.0.0:443.*ESTABLISHED/, Rush.bash("netstat -tan").split("\n"))
+    frontend.connection_status += eatme(/(0.0.0.0:80.*ESTABLISHED)/, Rush.bash("netstat -tan").split("\n"))
+    frontend.connection_status += eatme(/(0.0.0.0:443.*ESTABLISHED)/, Rush.bash("netstat -tan").split("\n"))
     frontend.save
 
     for be in frontend.backends do
-      be.listen_status += eatme(/#{be.cluster_address}:#{be.cluster_port}.*LISTEN/, Rush.bash("netstat -tn").split("\n"))
-      be.connection_status += eatme(/#{be.address}:#{be.port}.*ESTABLISHED/, Rush.bash("netstat -tn").split("\n"))
+      be.listen_status += eatme(/(#{be.cluster_address}:#{be.cluster_port}.*LISTEN)/, Rush.bash("netstat -tn").split("\n"))
+      be.connection_status += eatme(/(#{be.address}:#{be.port}.*ESTABLISHED)/, Rush.bash("netstat -tn").split("\n"))
       be.save
     end
   else
