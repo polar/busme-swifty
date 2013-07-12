60457f09c2f0d6d4e9ec04777fb9e6631d6321ab
Polar Humenn <polar@adiron.com>
2013-07-12 15:50:33 -0400

    update

--- scripts/create_backend_configfiles.sh
+++ scripts/create_backend_configfiles.sh
@@ -26,8 +26,36 @@ mkdir -p ./backends.d
 
 echo "Creating ./backends.d/$NAME.conf"
 
-if [ ! -e ./backends.d/$NAME.conf ]; then
+if [ "x$MASTER_SLUG" = "x" ]; then
+    sed -e "s/@MASTER_SLUG/$MASTER_SLUG/g" base-backend-nginx.conf.template |\
+    sed -e "s/@NAME/$NAME/g" |\
+    sed -e "s/@FRONTEND_ADDRESS/$FRONTEND_ADDRESS/g" |\
+    sed -e "s/@HOSTNAME/$HOSTNAME/g" | \
+    sed -e "s/@SERVER_NAME/$SERVER_NAME/g" | \
+    sed -e "s/@CLUSTER_ADDRESS/$CLUSTER_ADDRESS/g" |\
+    sed -e "s/@CLUSTER_PORT/$CLUSTER_PORT/g" |\
+    sed -e "s/@BACKEND_ADDRESS/$BACKEND_ADDRESS/g" |\
+    sed -e "s/@BACKEND_PORT/$BACKEND_PORT/g" |\
+    sed -e "s/@SSL_CERT/$SSL_CERT/g" | \
+    sed -e "s/@SSL_KEY/$SSL_KEY/g" > ./backends.d/$NAME.conf
+
+else
+
     sed -e "s/@MASTER_SLUG/$MASTER_SLUG/g" backend-nginx.conf.template |\
+    sed -e "s/@NAME/$NAME/g" |\
+    sed -e "s/@FRONTEND_ADDRESS/$FRONTEND_ADDRESS/g" |\
+    sed -e "s/@HOSTNAME/$HOSTNAME/g" | \
+    sed -e "s/@SERVER_NAME/$SERVER_NAME/g" | \
+    sed -e "s/@CLUSTER_ADDRESS/$CLUSTER_ADDRESS/g" |\
+    sed -e "s/@CLUSTER_PORT/$CLUSTER_PORT/g" |\
+    sed -e "s/@BACKEND_ADDRESS/$BACKEND_ADDRESS/g" |\
+    sed -e "s/@BACKEND_PORT/$BACKEND_PORT/g" |\
+    sed -e "s/@SSL_CERT/$SSL_CERT/g" | \
+    sed -e "s/@SSL_KEY/$SSL_KEY/g" > ./backends.d/$NAME.conf
+
+    echo "Creating ./backends.d/$NAME.location"
+
+    sed -e "s/@MASTER_SLUG/$MASTER_SLUG/g" backend-nginx.location.template |\
     sed -e "s/@NAME/$NAME/g" |\
     sed -e "s/@FRONTEND_ADDRESS/$FRONTEND_ADDRESS/g" |\
     sed -e "s/@HOSTNAME/$HOSTNAME/g" | \
@@ -44,7 +72,6 @@ mkdir -p ./start.d
 
 echo "Creating ./start.d/$NAME.sh"
 
-if [ ! -e ./start.d/$NAME.sh ];then
     sed -e "s/@MASTER_SLUG/$MASTER_SLUG/g" backend-start.sh.template |\
     sed -e "s/@NAME/$NAME/g" |\
     sed -e "s/@FRONTEND_ADDRESS/$FRONTEND_ADDRESS/g" |\
@@ -56,7 +83,6 @@ if [ ! -e ./start.d/$NAME.sh ];then
     sed -e "s/@BACKEND_PORT/$BACKEND_PORT/g" |\
     sed -e "s/@SSL_CERT/$SSL_CERT/g" | \
     sed -e "s/@SSL_KEY/$SSL_KEY/g" > ./start.d/$NAME.sh
-fi
 
 chmod +x ./start.d/$NAME.sh
 
