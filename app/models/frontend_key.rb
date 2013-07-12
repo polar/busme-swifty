eb40b747a054a941e87779eb5b42fd15a9d5fcdb
Polar Humenn <polar@adiron.com>
2013-07-12 14:58:11 -0400

    update

--- app/models/frontend_key.rb
+++ app/models/frontend_key.rb
@@ -1,3 +1,8 @@
+
+#
+# This class is only a partial of the real in buspass-web
+# because it carries CarrierWave and we don't need that here.
+#
 class FrontendKey
   include MongoMapper::Document
 
