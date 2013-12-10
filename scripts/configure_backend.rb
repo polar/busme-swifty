3bcbecdb2df61cb84737290cfa26373e539f62aa
Polar Humenn <polar@adiron.com>
2013-12-10 16:57:30 -0500

    update

--- scripts/configure_backend.rb
+++ scripts/configure_backend.rb
@@ -21,7 +21,7 @@ require File.expand_path("../config/initialize.rb", File.dirname(__FILE__))
     locations = backend.frontend.backends.map do |be|
       be.locations.map do |location|
     "
-          location ~ /#{location.gsub("/", "\\/")}/ {
+          location ^~ /#{location}/ {
               proxy_set_header  X-Real-IP  $remote_addr;
               proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header  Host $http_host;
